065bc905dab60c7f59e99166bd32b649ce0d5603
cyrilvincent <contact@cyrilvincent.com>
2020-02-11 20:32:29 +0100

    multi channels

--- demokeras.py
+++ demokeras.py
@@ -4,7 +4,6 @@ trainset = keras.preprocessing.image.ImageDataGenerator(rescale=1./255, validati
 #validationset = keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
 batchSize = 4
 nbSample = 287
-# validationSize = 260
 
 # Image with 2 channels : https://github.com/keras-team/keras/issues/3416
 # https://stackoverflow.com/questions/49404993/keras-how-to-use-fit-generator-with-multiple-inputs
@@ -16,6 +15,17 @@ trainGenerator = trainset.flow_from_directory(
         color_mode="grayscale",
         batch_size=batchSize,
         class_mode="categorical",
+        seed=1
+        )
+
+trainGenerator2 = trainset.flow_from_directory(
+        r'C:\Users\conta\CVC\ATP\Mesulog\191219_baseImages_tester\_BaseReference',
+        target_size=(64, 64),
+        subset = 'training',
+        color_mode="grayscale",
+        batch_size=batchSize,
+        class_mode="categorical",
+        seed=1
         )
 
 # validationGenerator = validationset.flow_from_directory(
@@ -32,11 +42,36 @@ validationGenerator = trainset.flow_from_directory(
         color_mode="grayscale",
         batch_size=batchSize,
         class_mode="categorical",
+        seed=1
         )
+
+validationGenerator2 = trainset.flow_from_directory(
+        r'C:\Users\conta\CVC\ATP\Mesulog\191219_baseImages_tester\_BaseReference',
+        target_size=(64, 64),
+        subset = 'validation',
+        color_mode="grayscale",
+        batch_size=batchSize,
+        class_mode="categorical",
+        seed=1
+        )
+
+import numpy as np
+def twoImageGenerator(generator1, generator2):
+    while True:
+        X1i = generator1.next()
+        X2i = generator2.next()
+        imgFirst = X1i[0]
+        imgSecond = X2i[0]
+        imgi = np.zeros((len(imgFirst), 64, 64, 2), dtype=np.float32)
+        for n in range(0, len(imgFirst)):
+            img1 = imgFirst[n]
+            img2 = imgSecond[n]
+            imgi[n] = np.dstack((img1, img2))
+        yield imgi, X2i[1]  # Yield both images and their mutual label
 
 #CNN
 model = keras.models.Sequential()
-model.add(keras.layers.Conv2D(16, (3, 3), input_shape=(64, 64, 1)))
+model.add(keras.layers.Conv2D(16, (3, 3), input_shape=(64, 64, 2)))
 model.add(keras.layers.Activation('relu'))
 model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
 # 32, 32, 32
@@ -72,11 +107,29 @@ model.compile(loss='categorical_crossentropy',
 #         validation_steps=validationSize // batchSize
 # )
 
+# model.fit(
+#             trainGenerator,
+#             epochs=30,
+#             validation_data=validationGenerator,
+#     )
+
 model.fit(
-            trainGenerator,
+            twoImageGenerator(trainGenerator, trainGenerator2),
             epochs=30,
-            validation_data=validationGenerator,
+            validation_data=twoImageGenerator(validationGenerator, validationGenerator2),
+            steps_per_epoch=nbSample // batchSize,
+            validation_steps=(nbSample * 0.2) // batchSize
     )
+
+# Other method
+#PIL Merge
+# from PIL import Image
+# imr = Image.open("rpath")
+# img =  Image.open("gpath")
+# imb = Image.open("bpath")
+# im = Image.merge("RGB", (imr, img, imb))
+# im.save("rgbpath")
+
 
 model.save('cnnmodel.h5')
 
