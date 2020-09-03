abceb9a3bd97bb5e10c51a4719c1c9570eeb3419
cyrilvincent <contact@cyrilvincent.com>
2020-09-03 22:47:11 +0200

    pytransform bug

--- DL/compile.bat
+++ DL/compile.bat
@@ -1,4 +1,7 @@
 pyarmor o predict.py
 pyinstaller --noconfirm predict.spec
 copy mv*.dll dist\predict
+md dist\predict\pytransform\platforms\windows\x86_64
+copy dist\pytransform\_pytransform.dll dist\predict\pytransform\platforms\windows\x86_64
+copy dist\pytransform\_pytransform.dll dist\predict
 
